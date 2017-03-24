0b169a479300a5925372199a60157ee12a62e79b
conorbroderick <cjayjayb@gmail.com>
2017-03-24 17:40:13 +0000

    updated to latest dominos code

--- app.py
+++ app.py
@@ -39,12 +39,12 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    if "compliment" in message_text:
+                    if "compliment" or "Compliment" in message_text:
                         send_message(sender_id, "You're looking well today :)")
                     elif "the best?" in message_text:
                         send_message(sender_id, "Aisling's the best <3")
                     else:
-                        send_message(sender_id, "Hi there, the current Dominos Pizza Discount code is: 6NATIONS")
+                        send_message(sender_id, "Hi there, the current Dominos Pizza Discount code is: SPRING77")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
