3d777996c954189443a761ec72753ecd55b873ce
conorbroderick <cjayjayb@gmail.com>
2017-09-16 19:21:08 +0100

    new code

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__,
             static_folder='web/static',
             template_folder='web/templates')
 
-LATEST_CODE = "ROI30OFF"
+LATEST_CODE = "HAMROCK1"
 
 @app.route('/', methods=['GET'])
 def verify():
