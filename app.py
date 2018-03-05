51c42c56151479e2b4f43de266858b2c9b35b1a7
conorbroderick <cjayjayb@gmail.com>
2018-03-05 18:27:50 +0000

    fix code for feb march

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__,
             static_folder='web/static',
             template_folder='web/templates')
 
-LATEST_CODE = "RUGBY2018"
+LATEST_CODE = "RUGBY018"
 
 @app.route('/', methods=['GET'])
 def verify():
